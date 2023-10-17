b0db97876b38ab015f6b0c72adefe4bc1a317715
Sandra Lillepool <sandra.lillepool@voco.ee>
2023-10-17 14:46:47 +0300

    todolist tasks

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const fs = require('fs');
 
 // use ejs filed to prepare templates for views
 const path = require('path')
@@ -7,8 +8,25 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname))
 
 app.get('/', (req, res) => {
- res.render('index')
- });
+	// get the data
+ fs.readFile('./tasks', 'utf8', (err, data) => {
+ 	if (err) {
+ 		console.error(err);
+ 		return;
+ 	}
+ 	console.log(data);
+ 	console.log(typeof data);
+ 	console.log(data.split("\n"))
+ 	//tasks list from the data
+ 	const tasks = ('Study CSS', 'Prepare presentation', 'Finish first web app', 'Have a cookie')
+ 	res.render('index', {tasks: tasks})
+ 	});
+ })
+
+app.post('/', (reg, res) => { 
+	console.log('form and data')
+	});
+
 
 app.listen(3001, () => {
 console.log('Example app is started at http://localhost:3001')
