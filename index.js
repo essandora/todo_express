3addfee57d9f17ae44475d5bfaef4fc88a9b9e7d
Sandra Lillepool <sandra.lillepool@voco.ee>
2023-11-02 22:41:33 +0200

    write data to file

--- index.js
+++ index.js
@@ -7,24 +7,48 @@ const path = require('path')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname))
 
+const readFile = (filename) => {
+	return new Promise ((resolve, reject) => {
+		// get data from file
+		fs.readFile(filename, 'utf8', (err, data) => {
+			if (err) {
+				console.error(err);
+				return
+			}
+			//tasks list data from file 
+			const tasks = data.split("\n") 
+			resolve(tasks)
+			});
+		})
+	}
+
 app.get('/', (req, res) => {
-	// get the data
- fs.readFile('./tasks', 'utf8', (err, data) => {
- 	if (err) {
- 		console.error(err);
- 		return;
- 	}
- 	//tasks list from the data
- 	const tasks = data.split("\n")
- 	res.render('index', {tasks: tasks})
- 	});
- })
-
-app.post('/', (reg, res) => { 
-	console.log('form sent data')
+	// tasks list data from file
+	readFile('./tasks')
+		.then(tasks => {
+			console.log(tasks)
+			res.render('index', {tasks: tasks})
 	})
+})
 
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/', (req, res) => { 
+	readFile('./tasks')
+	.then(tasks => {
+		tasks.push(req.body.task)
+		const data = tasks.join("\n")
+		fs.writeFile('./tasks', data, err => {
+			if (err) {
+				console.error(err);
+				return
+			}
+			//redirect to see result
+			res.redirect('/')
+		})
+	})
+})
 
 app.listen(3001, () => {
 console.log('Example app is started at http://localhost:3001')
-}) 
+});
